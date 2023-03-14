0162979ae1694d0949bdc82a2cacc23c01595af4
DamianDib9 <121461459+DamianDib9@users.noreply.github.com>
2023-03-14 17:08:20 -0300

    Update script.js
    Added pop up menu function to mobile version

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-const menuBtn = document.getElementById("menuBtn")
+document.querySelector('#menu').onclick = function () {
+  const popUpNav = document.querySelector('#popMenuCont');
+  popUpNav.style.display = 'flex';
+};
+
+document.querySelector('#cross').onclick = function () {
+  const popOutNav = document.querySelector('#popMenuCont');
+  popOutNav.style.display = 'none';
+};
+
+document.querySelector('#popHello').onclick = function () {
+  const goToSection = document.querySelector('#popMenuCont');
+  goToSection.style.display = 'none';
+};
+
+document.querySelector('#popPortfolio').onclick = function () {
+  const goToSection = document.querySelector('#popMenuCont');
+  goToSection.style.display = 'none';
+};
+document.querySelector('#poppAbout').onclick = function () {
+  const goToSection = document.querySelector('#popMenuCont');
+  goToSection.style.display = 'none';
+};
+document.querySelector('#poppContact').onclick = function () {
+  const goToSection = document.querySelector('#popMenuCont');
+  goToSection.style.display = 'none';
+};
