a7c01d850abd14e89497727a5ea34a2d04d2f404
DamianDib9 <121461459+DamianDib9@users.noreply.github.com>
2023-03-14 23:06:57 -0300

    Update script.js
    Fixed some minor typo errors

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 // When the menu button is clicked, display the pop-up navigation
 document.querySelector('#menu').onclick = function () {
-const popUpNav = document.querySelector('#popMenuCont');
-popUpNav.style.display = 'flex';
-}
+  const popUpNav = document.querySelector('#popMenuCont');
+  popUpNav.style.display = 'flex';
+};
 
 // When the cross button is clicked, hide the pop-up navigation
 document.querySelector('#cross').onclick = function () {
-const popOutNav = document.querySelector('#popMenuCont');
-popOutNav.style.display = 'none';
-}
+  const popOutNav = document.querySelector('#popMenuCont');
+  popOutNav.style.display = 'none';
+};
 
 // When the "Hello" button is clicked, hide the pop-up navigation
 document.querySelector('#popHello').onclick = function () {
-const goToSection = document.querySelector('#popMenuCont');
-goToSection.style.display = 'none';
-}
+  const goToSection = document.querySelector('#popMenuCont');
+  goToSection.style.display = 'none';
+};
 
 // When the "Portfolio" button is clicked, hide the pop-up navigation
 document.querySelector('#popPortfolio').onclick = function () {
-const goToSection = document.querySelector('#popMenuCont');
-goToSection.style.display = 'none';
-}
+  const goToSection = document.querySelector('#popMenuCont');
+  goToSection.style.display = 'none';
+};
 
 // When the "About" button is clicked, hide the pop-up navigation
 document.querySelector('#poppAbout').onclick = function () {
-const goToSection = document.querySelector('#popMenuCont');
-goToSection.style.display = 'none';
-}
+  const goToSection = document.querySelector('#popMenuCont');
+  goToSection.style.display = 'none';
+};
 
 // When the "Contact" button is clicked, hide the pop-up navigation
 document.querySelector('#poppContact').onclick = function () {
-const goToSection = document.querySelector('#popMenuCont');
-goToSection.style.display = 'none';
-}
+  const goToSection = document.querySelector('#popMenuCont');
+  goToSection.style.display = 'none';
+};
