4fd4ade9798bc7b438800168f1fabfc4fd197860
DamianDib9 <121461459+DamianDib9@users.noreply.github.com>
2023-03-17 19:59:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -143,3 +143,111 @@ button.forEach((i, indice) => {
 });
 
 // REPETICION
+
+const popupProjectsDesk = [{
+  picture: 'img/HQproject1.svg',
+  projectName: 'Project 1',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+  languages: ['Html/CSS', 'Ruby on rails', 'JavaScrip'],
+  seeLink: 'https://damiandib9.github.io/averger/',
+  sourceLink: 'https://github.com/DamianDib9/averger',
+},
+
+{
+  picture: 'img/HQproject2.svg',
+  projectName: 'Project 2',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+  languages: ['Html/CSS', 'Ruby on rails', 'JavaScrip'],
+  seeLink: 'https://damiandib9.github.io/averger/',
+  sourceLink: 'https://github.com/DamianDib9/averger',
+},
+
+{
+  picture: 'img/HQproject3.svg',
+  projectName: 'Project 3',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+  languages: ['Html/CSS', 'Ruby on rails', 'JavaScrip'],
+  seeLink: 'https://damiandib9.github.io/averger/',
+  sourceLink: 'https://github.com/DamianDib9/averger',
+},
+
+{
+  picture: 'img/HQproject4.svg',
+  projectName: 'Project 4',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+  languages: ['Html/CSS', 'Ruby on rails', 'JavaScrip'],
+  seeLink: 'https://damiandib9.github.io/averger/',
+  sourceLink: 'https://github.com/DamianDib9/averger',
+},
+
+{
+  picture: 'img/HQproject5.svg',
+  projectName: 'Project 5',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+  languages: ['Html/CSS', 'Ruby on rails', 'JavaScrip'],
+  seeLink: 'https://damiandib9.github.io/averger/',
+  sourceLink: 'https://github.com/DamianDib9/averger',
+},
+
+{
+  picture: 'img/HQproject6.svg',
+  projectName: 'Project 6',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+  languages: ['Html', 'Ruby on rails', 'JavaScrip', 'CSS'],
+  seeLink: 'https://damiandib9.github.io/averger/',
+  sourceLink: 'https://github.com/DamianDib9/averger',
+},
+];
+
+const button2 = document.querySelectorAll('.STP-button');
+const popupContainerDesk = document.querySelector('.popupContainer2');
+
+button.forEach((i, indice2) => {
+  i.addEventListener('click', () => {
+    const damian2 = `<div class="popupTextBox">
+          <div class="popboxTitle">
+              <h3 id="crossTypo2" class="popProjectTitle"> X </h3>
+              <h3 class="popProjectTitle"> ${popupProjectsDesk[indice2].projectName} </h3>
+          </div>
+          <div class="popCodingLangButtons">
+              <ul class="lists">
+                  <li class="popupLang">${popupProjectsDesk[indice2].languages[0]}</li>
+                  <li class="popupLang">${popupProjectsDesk[indice2].languages[1]}</li>
+                  <li class="popupLang">${popupProjectsDesk[indice2].languages[2]}</li>
+              </ul>
+          </div>
+      </div>
+  
+      <div class="popPicture">
+          <img class="popPicturesPreview" src="${popupProjectsDesk[indice2].picture}" alt="Project Number 1">
+      </div>
+  
+      <div id="popupPdiv">
+          <p class="popupP">
+          ${popupProjectsDesk[indice2].description}
+          </p>
+  
+          <div class="inlineBlockButton">
+          <a href="${popupProjectsDesk[indice2].seeLink}">
+              <button type="button" id="popUnionButton" class="popupButtons">
+                  <h4 class="popButText">See live</h4>
+              </button>
+              </a>
+
+              <a href="${popupProjectsDesk[indice2].sourceLink}">
+              <button type="button" id="popGitHubButton" class="popupButtons">
+                  <h4 class="popButText"> See source</h4>
+              </button>
+              </a>
+          </div>
+      </div>`;
+
+    popupContainerDesk.innerHTML = damian2;
+    popupContainerDesk.classList.add('popup2');
+
+    const closex2 = document.getElementById('crossTypo2');
+    closex2.addEventListener('click', () => {
+      popupContainerDesk.classList.remove('popup2');
+    });
+  });
+});
