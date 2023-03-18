852b296e4d88b215bfdda190234565ebf1499bba
DamianDib9 <121461459+DamianDib9@users.noreply.github.com>
2023-03-18 20:34:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -159,7 +159,7 @@ const popupProjectsDesk = [{
   description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
   languages: ['Html', 'Ruby on rails', 'JavaScrip', 'CSS'],
   seeLink: 'https://damiandib9.github.io/averger/',
-  sourceLink: 'https://github.com/DamianDib9/averger'
+  sourceLink: 'https://github.com/DamianDib9/averger',
 },
 
 {
